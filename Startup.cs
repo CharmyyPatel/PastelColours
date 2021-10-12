1ae0e1aeb15a6f52680dca35008851724e2302b5
CharmyyPatel <90396296+CharmyyPatel@users.noreply.github.com>
2021-10-12 02:33:16 -0300

    Assignment3 ASP.NET

--- Startup.cs
+++ Startup.cs
@@ -14,26 +14,38 @@ namespace PastelColours
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            if (Environment.IsDevelopment())
+            {
+                services.AddDbContext<colourContext>(options =>
+                options.UseSqlite(
+                    Configuration.GetConnectionString("colourContext")));
+            }
+            else
+            {
+                services.AddDbContext<colourContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("coloursContext")));
+            }
             services.AddRazorPages();
-
-            services.AddDbContext<colourContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("colourContext")));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -49,7 +61,7 @@ namespace PastelColours
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            // app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
